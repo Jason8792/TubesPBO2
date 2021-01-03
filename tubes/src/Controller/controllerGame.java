c4238417ad2ff7cb7cd80843b2bffd4e8debf98b
Jason8792 <71681190+Jason8792@users.noreply.github.com>
2021-01-04 00:17:44 +0700

    Update Game controller

--- tubes/src/Controller/controllerGame.java
+++ tubes/src/Controller/controllerGame.java
@@ -15,6 +15,7 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.Random;
 
 public class controllerGame {
@@ -37,6 +38,10 @@ public class controllerGame {
     float damage;
     float curHpPc;
     float curHpPlayer;
+    float curmons1;
+    float curmons2;
+    float curmons3;
+    float multi;
 
     public ObservableList<Owned> monsterpc() {
             for (int i = 0; i < 3; i++) {
@@ -49,8 +54,14 @@ public class controllerGame {
         //buat player
         this.user = user;
         mymonster.setItems(olist);
+        curmons1 = olist.get(0).getMonsterByMonsterId1().getMaxHp();
+        curmons2 = olist.get(1).getMonsterByMonsterId1().getMaxHp();
+        curmons3 = olist.get(2).getMonsterByMonsterId1().getMaxHp();
+
         mymonster.getSelectionModel().select(0);
-        hptotalplayer.setText(mymonster.getValue().getMonsterByMonsterId1().getMaxHp().toString());
+        hptotalplayer.setText(String.valueOf(curmons1));
+        skill1.setText(mymonster.getValue().getMonsterskill1().getName());
+        skill2.setText(mymonster.getValue().getMonsterskill2().getName());
         currenthelathplayer.setText(mymonster.getValue().getMonsterByMonsterId1().getMaxHp().toString());
         attackplayer.setText(mymonster.getValue().getMonsterByMonsterId1().getAtk().toString());
         playerusername.setText(this.user.getUsername());
@@ -62,15 +73,17 @@ public class controllerGame {
     public void setMonsterPc(){
         Computer.setText(npclist.get(0).getMonsterByMonsterId1().getName());
         currenthealthpc.setText(npclist.get(0).getMonsterByMonsterId1().getMaxHp().toString());
+        System.out.println(npclist.get(0).getMonsterByMonsterId1().getMaxHp().toString());
         hptotalpc.setText(npclist.get(0).getMonsterByMonsterId1().getMaxHp().toString());
         attackpc.setText(npclist.get(0).getMonsterByMonsterId1().getAtk().toString());
     }
 
 
     public void enemyattack() throws IOException {
+
         Element eplayer = mymonster.getValue().getMonsterByMonsterId1().getElementmonster();
         Element eenemy = npclist.get(0).getMonsterByMonsterId1().getElementmonster();
-        float multiplier = (float) elementCheck(eenemy,eplayer);
+        float multiplier = elementCheck(eplayer,eenemy);
         String comment = komentar(multiplier);
         float curAtkPc = Float.parseFloat(attackpc.getText());
         curHpPlayer = Float.parseFloat(currenthelathplayer.getText());
@@ -78,101 +91,94 @@ public class controllerGame {
         int random = rand.nextInt(2);
         float purenpc;
         if(random==0) {
-            purenpc = curAtkPc * (npclist.get(0).getMonsterskill1().getEffect());
+            purenpc = curAtkPc + npclist.get(0).getMonsterskill1().getEffect();
+
             historyArea.appendText("PC  commanded "+ npclist.get(0).getMonsterByMonsterId1().getName()+
                     "\nto use Skill "+npclist.get(0).getMonsterskill1().getName()+"\n");
         }
         else{
-            purenpc = curAtkPc * (npclist.get(0).getMonsterskill2().getEffect());
+            purenpc = curAtkPc + npclist.get(0).getMonsterskill2().getEffect();
             historyArea.appendText("PC  commanded "+ npclist.get(0).getMonsterByMonsterId1().getName()+
                     "\nto use Skill "+npclist.get(0).getMonsterskill2().getName()+"\n");
+
         }
-        damage = ((purenpc- curDefPlayer)*multiplier);
-        if(multiplier==0){
-            damage = purenpc-curDefPlayer;
-        }
-        if(damage<0){
-            comment = comment + "Player's def has nullified enemy's damage";
-        }
-        if(curHpPc-damage<0){
-            curHpPlayer=(float)0.0;
+
+        damage = purenpc * multiplier - curDefPlayer;
+        historyArea.appendText(npclist.get(0).getMonsterByMonsterId1().getName() + " dealt " + damage + " " + eenemy.getElement()
+                + " damage to \n"+ mymonster.getValue().getMonsterByMonsterId1().getName()+"! " + comment+"\n");
+        if(curHpPlayer - damage <=0){
+            curHpPlayer=0.0f;
             actionplayer();
         }
         else{
-            curHpPlayer = curHpPlayer -damage;
+            curHpPlayer = curHpPlayer - damage;
+            currenthelathplayer.setText(String.valueOf(curHpPlayer));
         }
-        currenthelathplayer.setText(String.valueOf(curHpPc));
 
-        historyArea.appendText(npclist.get(0).getMonsterByMonsterId1().getName() + " dealt " + damage + " " + eenemy.getElement()
-                + " damage to \n"+ mymonster.getValue().getMonsterByMonsterId1().getName()+"! " + comment+"\n");
-
+        if(mymonster.getSelectionModel().getSelectedIndex()==0){
+            this.curmons1 = Float.parseFloat (currenthelathplayer.getText());
+        }
+        else if(mymonster.getSelectionModel().getSelectedIndex()==1){
+            this.curmons2 = Float.parseFloat(currenthelathplayer.getText());
+        }
+        else{
+            this.curmons3 = Float.parseFloat(currenthelathplayer.getText());
+        }
     }
 
     public void useskill1(ActionEvent actionEvent) throws IOException {
         Element eplayer = mymonster.getValue().getMonsterByMonsterId1().getElementmonster();
         Element eenemy = npclist.get(0).getMonsterByMonsterId1().getElementmonster();
-        float multiplier = (float) elementCheck(eplayer,eenemy);
+        float multiplier = elementCheck(eenemy,eplayer);
         String comment = komentar(multiplier);
         float curAtkPlayer = Float.parseFloat(attackplayer.getText());
         curHpPc = Float.parseFloat(currenthealthpc.getText());
         float curDefPc = Float.valueOf(npclist.get(0).getMonsterByMonsterId1().getDef());
-        float pure = curAtkPlayer+(mymonster.getValue().getMonsterskill1().getEffect());
-        damage = ((pure - curDefPc)*multiplier);
-        if (multiplier == 0){
-            damage = pure - curDefPc;
-        }
-        if (damage<0){
-            damage = 1;
-            //komen-nya panjang2 dan gda pembatas
-            comment = comment + "Enemy's def has nullified your damage.\n";
-        }
-        if(curHpPc-damage<0){
-            curHpPc = (float) 0.0;
-            actionnpc();
-        }
-        else {
-            curHpPc = curHpPc - damage;
-        }
-        currenthealthpc.setText(String.valueOf(curHpPc));
+        float pure = curAtkPlayer + mymonster.getValue().getMonsterskill1().getEffect();
+
+        damage = pure*multiplier-curDefPc;
+
         historyArea.appendText("Player "+playerusername.getText()+" commanded "+ mymonster.getValue().getMonsterByMonsterId1().getName()+
                 "\nto use Skill "+mymonster.getValue().getMonsterskill1().getName()+"\n");
         historyArea.appendText(mymonster.getValue().getMonsterByMonsterId1().getName() + " dealt " + damage + " " + eplayer.getElement()
                 + " damage to \n"+ npclist.get(0).getMonsterByMonsterId1().getName()+"! " + comment+"\n");
-        enemyattack();
-    }
-
-    public void useskill2(ActionEvent actionEvent) throws IOException {
-        actionnpc();
-        Element eplayer = mymonster.getValue().getMonsterByMonsterId1().getElementmonster();
-        Element eenemy = npclist.get(0).getMonsterByMonsterId1().getElementmonster();
-        float multiplier = (float) elementCheck(eplayer,eenemy);
-        String comment = komentar(multiplier);
-        float curAtkPlayer = Float.parseFloat(attackplayer.getText());
-        curHpPc = Float.parseFloat(currenthealthpc.getText());
-        float curDefPc = Float.valueOf(npclist.get(0).getMonsterByMonsterId1().getDef());
-        float pure = curAtkPlayer+(mymonster.getValue().getMonsterskill2().getEffect());
-        damage = ((pure - curDefPc)*multiplier);
-        if (multiplier == 0){
-            damage = pure - curDefPc;
-        }
-        if (damage<0){
-            damage = 1;
-            //komen-nya panjang2 dan gda pembatas
-            comment = comment + "Enemy's def has nullified your damage.";
-        }
-        if(curHpPc-damage<0){
-            curHpPc = (float) 0.0;
+        if(curHpPc-damage<=0){
+            curHpPc = 0.0f;
             actionnpc();
         }
         else {
             curHpPc = curHpPc - damage;
+            currenthealthpc.setText(String.valueOf(curHpPc));
+            enemyattack();
         }
-        currenthealthpc.setText(String.valueOf(curHpPc));
+    }
+
+    public void useskill2(ActionEvent actionEvent) throws IOException {
+        Element eplayer = mymonster.getValue().getMonsterByMonsterId1().getElementmonster();
+        Element eenemy = npclist.get(0).getMonsterByMonsterId1().getElementmonster();
+        float multiplier = elementCheck(eenemy,eplayer);
+        String comment = komentar(multiplier);
+        float curAtkPlayer = Float.parseFloat(attackplayer.getText());
+        curHpPc = Float.parseFloat(currenthealthpc.getText());
+        float curDefPc = Float.valueOf(npclist.get(0).getMonsterByMonsterId1().getDef());
+        float pure = curAtkPlayer+mymonster.getValue().getMonsterskill2().getEffect();
+        damage = pure * multiplier- curDefPc;
+
         historyArea.appendText("Player "+playerusername.getText()+" commanded "+ mymonster.getValue().getMonsterByMonsterId1().getName()+
                 "\nto use Skill "+mymonster.getValue().getMonsterskill2().getName()+"\n");
         historyArea.appendText(mymonster.getValue().getMonsterByMonsterId1().getName() + " dealt " + damage + " " + eplayer.getElement()
                 + " damage to \n"+ npclist.get(0).getMonsterByMonsterId1().getName()+"! " + comment+"\n");
-        enemyattack();
+
+        if(curHpPc-damage<=0){
+            curHpPc = 0.0f;
+            actionnpc();
+        }
+        else {
+            curHpPc = curHpPc - damage;
+            enemyattack();
+        }
+        currenthealthpc.setText(String.valueOf(curHpPc));
+
     }
 
     public void retreat(ActionEvent actionEvent) throws IOException {
@@ -190,38 +196,33 @@ public class controllerGame {
     }
 
     public void actionnpc() throws IOException {
-        curHpPc = Float.parseFloat(currenthealthpc.getText());
-        if (curHpPc == 0.0) {
-            if(!npclist.isEmpty()){
+        if(npclist.size()>1){
             historyArea.appendText(npclist.get(0).getMonsterByMonsterId1().getName()+" has reached 0 hp!\n");
             npclist.remove(0);
-            npclist.get(0);
             setMonsterPc();
             }
-            else{
+        else{
                 //save game disini
 
-                historyArea.appendText(playerusername.getText()+" Has been Win !");
-                Alert alert = new Alert(Alert.AlertType.INFORMATION);
-                alert.setTitle("Kamu Menang");
-                alert.setHeaderText("Selamat Kamu menang");
-                alert.setContentText("back to main menu");
-                alert.showAndWait();
-                Stage stage = (Stage) leavebattle.getScene().getWindow();
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/beranda.fxml"));
-                Parent root = loader.load();
-                stage.setTitle("Beranda");
-                stage.setScene(new Scene(root, 600, 400));
-                stage.show();
-            }
-
+            historyArea.appendText(playerusername.getText()+" Has been Win !");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Kamu Menang");
+            alert.setHeaderText("Selamat Kamu menang");
+            alert.setContentText("back to main menu");
+            alert.showAndWait();
+            Stage stage = (Stage) leavebattle.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/beranda.fxml"));
+            Parent root = loader.load();
+            stage.setTitle("Beranda");
+            stage.setScene(new Scene(root, 600, 400));
+            stage.show();
         }
+
     }
 
     public void actionplayer() throws IOException {
         curHpPlayer = Float.parseFloat(currenthelathplayer.getText());
-        if (curHpPlayer == 0.0) {
-            if(!mymonster.getItems().isEmpty()){
+            if(mymonster.getItems().size()>1){
                 historyArea.appendText(mymonster.getValue().getMonsterByMonsterId1().getName()+" has reached 0 hp!\n");
 
                 Owned deadmonster = mymonster.getSelectionModel().getSelectedItem();
@@ -231,6 +232,8 @@ public class controllerGame {
                 currenthelathplayer.setText(mymonster.getValue().getMonsterByMonsterId1().getMaxHp().toString());
                 attackplayer.setText(mymonster.getValue().getMonsterByMonsterId1().getAtk().toString());
                 playerusername.setText(this.user.getUsername());
+
+
 
             }
             else{
@@ -249,8 +252,6 @@ public class controllerGame {
                 stage.setScene(new Scene(root, 600, 400));
                 stage.show();
             }
-
-        }
     }
 
 
@@ -277,57 +278,96 @@ public class controllerGame {
         return npc;
     }
 
-
     public void switchmonster(ActionEvent actionEvent) {
+        if(mymonster.getSelectionModel().getSelectedIndex()==0){
+            currenthelathplayer.setText(String.valueOf(this.curmons1));
+        }
+        else if(mymonster.getSelectionModel().getSelectedIndex()==1){
+            currenthelathplayer.setText(String.valueOf(this.curmons2));
+        }
+        else {
+            currenthelathplayer.setText(String.valueOf(this.curmons3));
+        }
+        skill1.setText(mymonster.getValue().getMonsterskill1().getName());
+        skill2.setText(mymonster.getValue().getMonsterskill2().getName());
         hptotalplayer.setText(mymonster.getValue().getMonsterByMonsterId1().getMaxHp().toString());
-        currenthelathplayer.setText(mymonster.getValue().getMonsterByMonsterId1().getMaxHp().toString());
         attackplayer.setText(mymonster.getValue().getMonsterByMonsterId1().getAtk().toString());
     }
 
-    public double elementCheck(Element element1, Element element2) {
-            switch (element1.getElement()) {
+    public float elementCheck(Element element1, Element element2) {
+
+        System.out.println(element1.getElement());
+            switch (element1.getElement().toLowerCase()) {
                 case "water":
-                    switch (element2.getElement()) {
+                    switch (element2.getElement().toLowerCase()) {
                         case "electric":
-                            return 2.0;
+                            this.multi = 2.0f;
+                            break;
                         case "fire":
-                            return 0.5;
+                            this.multi = 0.5f;
+                            break;
+                        default:
+                            this.multi=1.0f;
+                            break;
                     }
                     break;
                 case "grass":
-                    switch (element2.getElement()) {
+                    switch (element2.getElement().toLowerCase()) {
                         case "fire":
-                            return 2.0;
+                            this.multi = 2.0f;
+                            break;
                         case "ground":
-                            return 0.5;
+                            this.multi = 0.5f;
+                            break;
+                        default:
+                            this.multi=1.0f;
+                            break;
                     }
                     break;
                 case "fire":
-                    switch (element2.getElement()) {
+                    switch (element2.getElement().toLowerCase()) {
                         case "water":
-                            return 2.0;
+                            this.multi = 2.0f;
+                            break;
                         case "grass":
-                            return 0.5;
+                            this.multi = 0.5f;
+                            break;
+                        default:
+                            this.multi=1.0f;
+                            break;
                     }
                     break;
                 case "ground":
-                    switch (element2.getElement()) {
+                    switch (element2.getElement().toLowerCase()) {
                         case "grass":
-                            return 2.0;
+                            this.multi = 2.0f;
+                            break;
                         case "electric":
-                            return 0.0;
+                            this.multi = 1.0f;
+                            break;
+                        default:
+                            this.multi=1.0f;
+                            break;
                     }
                     break;
                 case "electric":
-                    switch (element2.getElement()) {
+                    switch (element2.getElement().toLowerCase()) {
                         case "ground":
-                            return 2.0;
+                            this.multi = 2.0f;
+                            break;
                         case "water":
-                            return 0.5;
+                            this.multi = 0.5f;
+                            break;
+                        default:
+                            this.multi=1.0f;
+                            break;
                     }
                     break;
+
+
             }
-        return 0.0;
+            return this.multi;
+
     }
 
     public String komentar(float m){
